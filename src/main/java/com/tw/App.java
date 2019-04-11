da6b88ddf5b51ad8967be9cd2f3ed5532936b41d
superPershing <727342027@qq.com>
2019-04-11 16:32:24 +0800

    feat(App): complete functions in App.java

--- src/main/java/com/tw/App.java
+++ src/main/java/com/tw/App.java
@@ -1,23 +1,65 @@
 package com.tw;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class App implements Service {
+    private HashSet<Student> studentHashSet = new HashSet<>();
 
     private AddStudent addStudentService = new AddStudent();
     private BuildGradeTable buildGradeTableService = new BuildGradeTable();
 
+    public enum State {
+        ADD_STUDENT_INFO("1"), SHOW_GRADE_TABLE("2"), EXIT("3");
+        private String command;
+
+        State(String command) {
+            this.command = command;
+        }
+
+        static boolean isFormatCorrect(String code) {
+            return Arrays.stream(State.values()).anyMatch(x -> x.command == code);
+        }
+
+        static State getState(String code) {
+            for (State value : State.values()) {
+                if (value.command == code) {
+                    return value;
+                }
+            }
+            return null;
+        }
+    }
+
     @Override
     public boolean checkCommandFormat(String input) {
+        if (State.isFormatCorrect(input)) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public String generatePrompt(boolean isRight) {
-        return null;
+        return "1. 添加学生\n" +
+                "2. 生成成绩单\n" +
+                "3. 退出请输入你的选择（1～3）：\n";
     }
 
     @Override
     public void handleCommand(String command) {
-
+        State state = State.getState(command);
+        switch (state) {
+            case ADD_STUDENT_INFO:
+                addStudentService.main();
+                break;
+            case SHOW_GRADE_TABLE:
+                buildGradeTableService.main();
+                break;
+            case EXIT:
+                System.exit(0);
+                break;
+        }
     }
 
     public static void main(String[] args) {
